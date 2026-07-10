38a6aa6b39365f6d49206ab18bf62210e4d46f97
Mewan Aluth Baduge <a1236678@adelaide.edu.au>
2026-07-10 17:48:26 +0930

    Lesson 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,18 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularTraining');
+
+  protected readonly title = signal('angular-deep-dive');
+
+  data = {
+    title: 'taukanda title'
+  };
+
+  OnKeyUp(newTitle:string) {
+    this.data.title = newTitle;
+  }
 }
